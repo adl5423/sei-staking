f3b0b203b11f4cd408e9fcb8b60de904eb76c76c
adl5423 <aisenlopezjr@gmail.com>
2024-02-12 10:18:26 -0500

    Connect Wallet?

--- app/components/Terminal/TerminalHeader.jsx
+++ app/components/Terminal/TerminalHeader.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+"use client";
+
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  useCosmWasmClient,
+  useSigningCosmWasmClient,
+  useWallet,
+  WalletConnectButton,
+} from "@sei-js/react";
 
 function TerminalHeader() {
+  const { connectedWallet, accounts } = useWallet();
+
   return (
     <div>
-      <div className="flex justify-between items-center px-9">
+      <div className="flex items-center justify-between px-9">
         <div>
           <p className="text-xl">YūhiSei</p>
         </div>
         <div>
-          <button className="text-black bg-white rounded-xl px-10 py-2 my-10 hover:bg-[#a5a5a5] active:bg-[#cfcfcf]">
+          {/* <button className="text-black bg-white rounded-xl px-10 py-2 my-10 hover:bg-[#a5a5a5] active:bg-[#cfcfcf]">
             <p className="font-bold">Connect Wallet</p>
-          </button>
+          </button> */}
+          <WalletConnectButton />
         </div>
       </div>
     </div>
